22c982dce2b31f2319ee3d937419f9548a8d395a
bigzcoder <840350739@qq.com>
2019-10-30 21:11:21 +0800

    fix 线性表

--- C语言代码练习/tmp/test19.cpp
+++ C语言代码练习/tmp/test19.cpp
@@ -1,21 +1,49 @@
-//#define MAXSIZE 100
-//typedef struct{
-//	ElemType *elem;
-//	int length;
-//}SqList;
+typedef struct LNode{
+	ElemType data;
+	struct LNode *next;
+}*LinkList,LNode;
 
-Status InitList(){
-	L.elem=new ElemType[MAXSIZE];
-	if(!L.elem){
-		exit(OVERFLOW);
-	}
-	L.length=0;
+Status InitList(LinkList &L){
+	L=new LNode;
+	L->next=NULL;
 	return OK;
 }
 
-Status GetElem(SqList L,int i,ElemType &e){
-	if(i<1||i>Length){
+Status GetElem(LinkList L,int i,ElemType &e){
+	p=L->next;
+	j=1;
+	while((j<i)&&(p)){
+		p=p->next;
+		j++;
+	}
+	if(i<j||!p){
 		return ERROR;
 	}
-	e=L.elem[i-1];
+	e=p->data;
+	return OK;
+}
+
+LNode *LocateElem(LinkList L,ElemType e){
+	p=L->next;
+	while(p!=NULL&&p->data!=e){
+		p=p->next;
+	}
+	return p;
+}
+
+Status ListInsert(LinkList L,ElemType e,int i){
+	p=L;
+	j=0;
+	while(j<i-1&&p){
+		p=p->next;
+		++j
+	}	
+	if(i-1<j||!p){
+		return ERROR;
+	}
+	s=new LNode;
+	s->data=e;
+	s->next=p->next;
+	p->next=s;
+	return OK;
 }
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## 个人的私有代码训练仓库
 
-[线性表](/考研/专业课复习markdown/数据结构MK/dev调试说明/线性表.md)   
-[线性表的应用](/考研/专业课复习markdown/数据结构MK/dev调试说明/线性表的应用.md)   
-[栈和队列](/考研/专业课复习markdown/数据结构MK/dev调试说明/栈和队列.md)   
-[串](/考研/专业课复习markdown/数据结构MK/dev调试说明/串.md)   
-[树](/考研/专业课复习markdown/数据结构MK/dev调试说明/树.md)  
-[图](/考研/专业课复习markdown/数据结构MK/dev调试说明/图.md)  
-[查找](/考研/专业课复习markdown/数据结构MK/dev调试说明/查找.md)  
-[排序](/考研/专业课复习markdown/数据结构MK/dev调试说明/排序.md)  
+[线性表](/考研/专业课复习markdown/数据结构MK/dev调试说明/线性表.md)    
+[线性表的应用](/考研/专业课复习markdown/数据结构MK/dev调试说明/线性表的应用.md)    
+[栈和队列](/考研/专业课复习markdown/数据结构MK/dev调试说明/栈和队列.md)    
+[串](/考研/专业课复习markdown/数据结构MK/dev调试说明/串.md)    
+[树](/考研/专业课复习markdown/数据结构MK/dev调试说明/树.md)   
+[图](/考研/专业课复习markdown/数据结构MK/dev调试说明/图.md)   
+[查找](/考研/专业课复习markdown/数据结构MK/dev调试说明/查找.md)   
+[排序](/考研/专业课复习markdown/数据结构MK/dev调试说明/排序.md)   
